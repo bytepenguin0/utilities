d7c07962261c71c43d43d940a9ec0f0b1764bf39
bytepenguin0 <totreadrian@gmail.com>
2024-12-18 12:26:27 +0100

    fix

--- main.lua
+++ main.lua
@@ -4,13 +4,12 @@ return {
         local dragInput
         local dragStart
         local startPos
-    
+
         frame.InputBegan:Connect(function(input)
             if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and game:GetService("UserInputService"):GetFocusedTextBox() == nil then
                 dragToggle = true
-                dragStart = input.Position
+                dragStart = Vector2.new(input.Position.X, input.Position.Y)
                 startPos = frame.Position
-
                 input.Changed:Connect(function()
                     if input.UserInputState == Enum.UserInputState.End then
                         dragToggle = false
@@ -18,22 +17,24 @@ return {
                 end)
             end
         end)
-    
+
         frame.InputChanged:Connect(function(input)
             if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
                 dragInput = input
             end
         end)
-    
+
         game:GetService("UserInputService").InputChanged:Connect(function(input)
             if input == dragInput and dragToggle then
-                local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + input.Position - dragStart.X, startPos.Y.Scale, startPos.Y.Offset + input.Position - dragStart.Y)
+                local inputPos = Vector2.new(input.Position.X, input.Position.Y)
+                local delta = inputPos - dragStart
+                local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
 
                 if dragSpeed and typeof(dragSpeed) == "number" then
                     game:GetService("TweenService"):Create(frame, TweenInfo.new(dragSpeed), {["Position"] = position}):Play()
+                else
+                    frame.Position = position
                 end
-
-                frame.Position = position
             end
         end)
     end
