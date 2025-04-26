11a15b60becf7696ed81c7dd93a4bbcbaecba021
bytepenguin0 <totreadrian@gmail.com>
2025-04-26 16:22:03 +0200

    metatables dragify

--- main.lua
+++ main.lua
@@ -1,55 +1,62 @@
-return {
-    dragify = function(frame, dragSpeed)
-        local dragToggle
-        local dragInput
-        local dragStart
-        local startPos
+local utilities = {}
 
-        getgenv()._candrag = true
+utilities.dragify = {}
+utilities.dragify.__index = utilities.dragify
 
-        frame.InputBegan:Connect(function(input)
-            if not getgenv()._candrag then
-                return
-            end
+function utilities.dragify(data)
+    assert(data, "missing data")
 
-            if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and game:GetService("UserInputService"):GetFocusedTextBox() == nil then
-                dragToggle = true
-                dragStart = Vector2.new(input.Position.X, input.Position.Y)
-                startPos = frame.Position
-                input.Changed:Connect(function()
-                    if input.UserInputState == Enum.UserInputState.End then
-                        dragToggle = false
-                    end
-                end)
-            end
-        end)
+    assert(data["frame"] ~= nil, "missing 'frame'")
+    assert(data["canDrag"] ~= nil, "missing 'canDrag'")
 
-        frame.InputChanged:Connect(function(input)
-            if not getgenv()._candrag then
-                return
-            end
+    data = setmetatable(data, utilities.dragify)
 
-            if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-                dragInput = input
-            end
-        end)
+    local dragToggle, dragInput, dragStart, startPos
+    
+    data.frame.InputBegan:Connect(function(input)
+        if not data.canDrag then
+            return
+        end
 
-        game:GetService("UserInputService").InputChanged:Connect(function(input)
-            if not getgenv()._candrag then
-                return
-            end
-            
-            if input == dragInput and dragToggle then
-                local inputPos = Vector2.new(input.Position.X, input.Position.Y)
-                local delta = inputPos - dragStart
-                local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
-
-                if dragSpeed and typeof(dragSpeed) == "number" then
-                    game:GetService("TweenService"):Create(frame, TweenInfo.new(dragSpeed), {["Position"] = position}):Play()
-                else
-                    frame.Position = position
+        if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and game:GetService("UserInputService"):GetFocusedTextBox() == nil then
+            dragToggle = true
+            dragStart = Vector2.new(input.Position.X, input.Position.Y)
+            startPos = data.frame.Position
+            input.Changed:Connect(function()
+                if input.UserInputState == Enum.UserInputState.End then
+                    dragToggle = false
                 end
+            end)
+        end
+    end)
+
+    data.frame.InputChanged:Connect(function(input)
+        if not data.canDrag then
+            return
+        end
+
+        if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+            dragInput = input
+        end
+    end)
+
+    game:GetService("UserInputService").InputChanged:Connect(function(input)
+        if not data.canDrag then
+            return
+        end
+        
+        if input == dragInput and dragToggle then
+            local inputPos = Vector2.new(input.Position.X, input.Position.Y)
+            local delta = inputPos - dragStart
+            local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+
+            if data.dragSpeed and typeof(data.dragSpeed) == "number" then
+                game:GetService("TweenService"):Create(data.frame, TweenInfo.new(data.dragSpeed), {["Position"] = position}):Play()
+            else
+                data.frame.Position = position
             end
-        end)
-    end
-}
+        end
+    end)
+end
+
+return utilities
